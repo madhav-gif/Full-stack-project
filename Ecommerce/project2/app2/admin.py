771f28d0388f94d16ed3fb8ea320f3b13fb3bace
Madhav kumar <krmadhav312@gmail.com>
2026-02-08 12:49:02 +0530

    Fix admin 500 error and Product image inline setup

--- Ecommerce/project2/app2/admin.py
+++ Ecommerce/project2/app2/admin.py
@@ -1,19 +1,28 @@
 from django.contrib import admin
-from .models import Product, Cart, Order,OrderItem, ProductImage,Wishlist
+from .models import Product, Cart, Order, OrderItem, ProductImage, Wishlist
+
 
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 5
 
+
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["colors"].help_text = 'JSON format: ["Red", "Blue"]'
+        form.base_fields["sizes"].help_text = 'JSON format: ["S", "M", "L"]'
+        return form
+
+
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields =('total_price','created_at')    
+    readonly_fields = ('total_price', 'created_at')
 
 
-admin.site.register(Product, ProductAdmin) 
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Cart)
-admin.site.register(Order,OrderAdmin)
-admin.site.register(ProductImage)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Wishlist)
 admin.site.register(OrderItem)
