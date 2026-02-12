2de0ce691ded654ef71e1ebdbbe96f8e5c83485b
Madhav kumar <krmadhav312@gmail.com>
2026-02-12 22:05:19 +0530

    added staticfiles back

--- Ecommerce/project2/project2/settings.py
+++ Ecommerce/project2/project2/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
 
     # Third-party
